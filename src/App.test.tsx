86a0c1b0c0262af7c513b0ffbbfdcf980463db4e
Jake Readman <jakereadman@gmail.com>
2022-08-14 12:48:50 +0100

    init commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import App, { replaceCamelWithSpaces } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial colour', () => {
+    render(<App />);
+    const btn = screen.getByRole('button', { name: 'Change to Midnight Blue' });
+    expect(btn).toHaveStyle({ backgroundColor: 'MediumVioletRed' });
+    fireEvent.click(btn);
+    expect(btn).toHaveStyle({ backgroundColor: 'MidnightBlue' });
+    expect(btn).toHaveTextContent('Change to Medium Violet Red');
+});
+
+test('initial conditions', () => {
+    render(<App />);
+    const btn = screen.getByRole('button', { name: 'Change to Midnight Blue' });
+    expect(btn).toBeEnabled();
+    const checkbox: HTMLInputElement = screen.getByRole('checkbox', {
+        name: 'Disable Button',
+    });
+    expect(checkbox.checked).toEqual(false);
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked;
+    expect(btn).toBeDisabled();
+    expect(btn).toHaveStyle({ backgroundColor: 'grey' });
+});
+
+describe('spaces before camel-case caps', () => {
+    test('works for no inner caps', () => {
+        expect(replaceCamelWithSpaces('red')).toBe('red');
+    });
+    test('works for one inner cap', () => {
+        expect(replaceCamelWithSpaces('midnightBlue')).toBe('midnight blue');
+    });
+    test('works for multiple caps', () => {
+        expect(replaceCamelWithSpaces('mediumVioletRed')).toBe(
+            'medium violet red'
+        );
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
+export const replaceCamelWithSpaces = (colourName: string): string => {
+    let colourNameArr: string[] = colourName.split('');
+    return colourNameArr
+        .map((letter, idx) => {
+            if (letter === letter.toUpperCase() && idx !== 0) {
+                return ` ${letter.toLowerCase()}`;
+            }
+            return letter;
+        })
+        .join('');
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const btnRef = useRef<HTMLButtonElement>(null);
+
+    const handleClick = (e: React.MouseEvent<HTMLElement>): void => {
+        e.preventDefault();
+        if (
+            btnRef.current!.style.backgroundColor.toLowerCase() ===
+            'mediumvioletred'
+        ) {
+            btnRef.current!.style.backgroundColor = 'MidnightBlue';
+            btnRef.current!.innerHTML = 'Change to Medium Violet Red';
+        } else {
+            btnRef.current!.style.backgroundColor = 'MediumVioletRed';
+            btnRef.current!.innerHTML = 'Change to Midnight Blue';
+        }
+    };
+
+    const toggleDisableBtn = (): void => {
+        btnRef.current!.disabled = !btnRef.current!.disabled;
+        if (btnRef.current!.disabled) {
+            btnRef.current!.style.backgroundColor = 'grey';
+            return;
+        }
+        if (btnRef.current!.innerHTML === 'Change to Medium Violet Red') {
+            btnRef.current!.style.backgroundColor = 'MidnightBlue';
+        } else {
+            btnRef.current!.style.backgroundColor = 'MediumVioletRed';
+        }
+    };
+
+    return (
+        <div className='App'>
+            <button
+                ref={btnRef}
+                style={{ backgroundColor: 'MediumVioletRed' }}
+                onClick={handleClick}
+            >
+                Change to Midnight Blue
+            </button>
+            <input
+                type='checkbox'
+                id='disable-btn-checkbox'
+                name='disable-btn-checkbox'
+                onClick={toggleDisableBtn}
+            />
+            <label htmlFor='disable-btn-checkbox'>Disable Button</label>
+        </div>
+    );
 }
 
 export default App;
